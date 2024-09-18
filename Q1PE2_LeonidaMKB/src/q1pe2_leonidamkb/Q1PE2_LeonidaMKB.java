8e95813d153df0f2ab6e57ead5b656ce41278c91
stewiestewsoup <143673362+stewiestewsoup@users.noreply.github.com>
2024-09-18 10:25:43 +0800

    Add files via upload

---
+++ Q1PE2_LeonidaMKB/src/q1pe2_leonidamkb/Q1PE2_LeonidaMKB.java
@@ -0,0 +1,220 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package q1pe2_leonidamkb;
+import java.util.*;
+
+/**
+ *
+ * @author PC7-COMLAB1
+ */
+public class Q1PE2_LeonidaMKB {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+    Scanner num = new Scanner(System.in);
+    System.out.print("Enter a number ");
+    int no = num.nextInt();
+            if (no == 1) {
+                System.out.print("One ");
+            } else if (no == 2) {
+                System.out.print("Two ");
+            } else if (no == 3) {
+                System.out.print("Three ");
+            } else if (no == 4) {
+                System.out.print("Four ");
+            } else if (no == 5) {
+                System.out.print("Five ");
+            } else if (no == 6) {
+                System.out.print("Six ");
+            } else if (no == 7) {
+                System.out.print("Seven ");
+            } else if (no == 8) {
+                System.out.print("Eight ");
+            } else if (no == 9) {
+                System.out.print("Nine ");
+            } else if (no == 10) {
+                System.out.print("Ten ");
+            } else if (no == 11) {
+                System.out.print("Eleven ");
+            } else if (no == 12) {
+                System.out.print("Twelve ");
+            } else if (no == 13) {
+                System.out.print("Thirteen ");
+            } else if (no == 14) {
+                System.out.print("Fourteen ");
+            } else if (no == 15) {
+                System.out.print("Fifteen ");
+            } else if (no == 16) {
+                System.out.print("Sixteen ");
+            } else if (no == 17) {
+                System.out.print("Seventeen ");
+            } else if (no == 18) {
+                System.out.print("Eighteen ");
+            } else if (no == 19) {
+                System.out.print("Nineteen ");
+            } else if (no == 20) {
+                System.out.print("Twenty ");
+            } else if (no == 21) {
+                System.out.print("Twenty-One ");
+            } else if (no == 22) {
+                System.out.print("Twenty-Two ");
+            } else if (no == 23) {
+                System.out.print("Twenty-Three ");
+            } else if (no == 24) {
+                System.out.print("Twenty-Four ");
+            } else if (no == 25) {
+                System.out.print("Twenty-Five ");
+            } else if (no == 26) {
+                System.out.print("Twenty-Six ");
+            } else if (no == 27) {
+                System.out.print("Twenty-Seven ");
+            } else if (no == 28) {
+                System.out.print("Twenty-Eight ");
+            } else if (no == 29) {
+                System.out.print("Twenty-Nine ");
+            } else if (no == 30) {
+                System.out.print("Thirty ");
+            } else if (no == 31) {
+                System.out.print("Thirty-One ");
+            } else if (no == 32) {
+                System.out.print("Thirty-Two ");
+            } else if (no == 33) {
+                System.out.print("Thirty-Three ");
+            } else if (no == 34) {
+                System.out.print("Thirty-Four ");
+            } else if (no == 35) {
+                System.out.print("Thirty-Five ");
+            } else if (no == 36) {
+                System.out.print("Thirty-Six ");
+            } else if (no == 37) {
+                System.out.print("Thirty-Seven ");
+            } else if (no == 38) {
+                System.out.print("Thirty-Eight ");
+            } else if (no == 39) {
+                System.out.print("Thirty-Nine ");
+            } else if (no == 40) {
+                System.out.print("Forty ");
+            } else if (no == 41) {
+                System.out.print("Forty-One ");
+            } else if (no == 42) {
+                System.out.print("Forty-Two ");
+            } else if (no == 43) {
+                System.out.print("Forty-Three ");
+            } else if (no == 44) {
+                System.out.print("Forty-Four ");
+            } else if (no == 45) {
+                System.out.print("Forty-Five ");
+            } else if (no == 46) {
+                System.out.print("Forty-Six ");
+            } else if (no == 47) {
+                System.out.print("Forty-Seven ");
+            } else if (no == 48) {
+                System.out.print("Forty-Eight ");
+            } else if (no == 49) {
+                System.out.print("Forty-Nine ");
+            } else if (no == 50) {
+                System.out.print("Fifty ");
+            } else if (no == 51) {
+                System.out.print("Fifty-One ");
+            } else if (no == 52) {
+                System.out.print("Fifty-Two ");
+            } else if (no == 53) {
+                System.out.print("Fifty-Three ");
+            } else if (no == 54) {
+                System.out.print("Fifty-Four ");
+            } else if (no == 55) {
+                System.out.print("Fifty-Five ");
+            } else if (no == 56) {
+                System.out.print("Fifty-Six ");
+            } else if (no == 57) {
+                System.out.print("Fifty-Seven ");
+            } else if (no == 58) {
+                System.out.print("Fifty-Eight ");
+            } else if (no == 59) {
+                System.out.print("Fifty-Nine ");
+            } else if (no == 60) {
+                System.out.print("Sixty ");
+            } else if (no == 61) {
+                System.out.print("Sixty-One ");
+            } else if (no == 62) {
+                System.out.print("Sixty-Two ");
+            } else if (no == 63) {
+                System.out.print("Sixty-Three ");
+            } else if (no == 64) {
+                System.out.print("Sixty-Four ");
+            } else if (no == 65) {
+                System.out.print("Sixty-Five ");
+            } else if (no == 66) {
+                System.out.print("Sixty-Six ");
+            } else if (no == 67) {
+                System.out.print("Sixty-Seven ");
+            } else if (no == 68) {
+                System.out.print("Sixty-Eight ");
+            } else if (no == 69) {
+                System.out.print("Sixty-Nine ");
+            } else if (no == 70) {
+                System.out.print("Seventy ");
+            } else if (no == 71) {
+                System.out.print("Seventy-One ");
+            } else if (no == 72) {
+                System.out.print("Seventy-Two ");
+            } else if (no == 73) {
+                System.out.print("Seventy-Three ");
+            } else if (no == 74) {
+                System.out.print("Seventy-Four ");
+            } else if (no == 75) {
+                System.out.print("Seventy-Five ");
+            } else if (no == 76) {
+                System.out.print("Seventy-Six ");
+            } else if (no == 77) {
+                System.out.print("Seventy-Seven ");
+            } else if (no == 78) {
+                System.out.print("Seventy-Eight ");
+            } else if (no == 79) {
+                System.out.print("Seventy-Nine ");
+            } else if (no == 80) {
+                System.out.print("Eighty ");
+            } else if (no == 81) {
+                System.out.print("Eighty-One ");
+            } else if (no == 82) {
+                System.out.print("Eighty-Two ");
+            } else if (no == 83) {
+                System.out.print("Eighty-Three ");
+            } else if (no == 84) {
+                System.out.print("Eighty-Four ");
+            } else if (no == 85) {
+                System.out.print("Eighty-Five ");
+            } else if (no == 86) {
+                System.out.print("Eighty-Six ");
+            } else if (no == 87) {
+                System.out.print("Eighty-Seven ");
+            } else if (no == 88) {
+                System.out.print("Eighty-Eight ");
+            } else if (no == 89) {
+                System.out.print("Eighty-Nine ");
+            } else if (no == 90) {
+                System.out.print("Ninety ");
+            } else if (no == 91) {
+                System.out.print("Ninety-One ");
+            } else if (no == 92) {
+                System.out.print("Ninety-Two ");
+            } else if (no == 93) {
+                System.out.print("Ninety-Three ");
+            } else if (no == 94) {
+                System.out.print("Ninety-Four ");
+            } else if (no == 95) {
+                System.out.print("Ninety-Five ");
+            } else if (no == 96) {
+                System.out.print("Ninety-Six ");
+            } else if (no == 97) {
+                System.out.print("Ninety-Seven ");
+            } else if (no == 98) {
+                System.out.print("Ninety-Eight ");
+            } else if (no == 99) {
+              System.out.print("Ninety-Nine");}
+    }
+}
